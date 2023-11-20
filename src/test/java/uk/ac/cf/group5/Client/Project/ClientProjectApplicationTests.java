3b6a115ec7fabb2b7ca81c6ffc77e5b0ef6cad4d
Clevis Gikenyi <gikenyic@cardiff.ac.uk>
2023-11-20 18:18:51 +0000

    initial commit

---
+++ src/test/java/uk/ac/cf/group5/Client/Project/ClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.group5.Client.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
