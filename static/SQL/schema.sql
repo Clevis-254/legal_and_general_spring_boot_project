0266c376a7c98276301fc6edb777231358039ff8
c22055810 <Cartwrighte2@cardiff.ac.uk>
2023-11-22 11:04:11 +0000

    Add schema to initialise database

---
+++ static/SQL/schema.sql
@@ -0,0 +1,26 @@
+DROP SCHEMA IF EXISTS `group_5_client_project` ;
+
+-- -----------------------------------------------------
+
+-- Create schema group_5_client_project
+
+-- -----------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS `group_5_client_project` DEFAULT CHARACTER SET utf8 ;
+
+USE `group_5_client_project` ;
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE adminUsers(
+    id INT NOT NULL AUTO_INCREMENT,
+    userID INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userID) REFERENCES users(id)
+);
