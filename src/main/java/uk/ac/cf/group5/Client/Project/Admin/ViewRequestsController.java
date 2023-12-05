d360bb6e2bd33a04f5ddb76cd76f0ee5d59ab99a
c21070842 <mogridgeb@gmail.com>
2023-12-05 15:27:46 +0000

    working review page loading for admin

--- src/main/java/uk/ac/cf/group5/Client/Project/Admin/ViewRequestsController.java
+++ src/main/java/uk/ac/cf/group5/Client/Project/Admin/ViewRequestsController.java
@@ -1,8 +1,10 @@
 package uk.ac.cf.group5.Client.Project.Admin;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class ViewRequestsController {
     @GetMapping("Admin/ViewRequests")
     public ModelAndView getViewRequests(){
