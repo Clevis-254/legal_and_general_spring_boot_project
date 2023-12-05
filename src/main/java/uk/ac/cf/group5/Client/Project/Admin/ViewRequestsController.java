8775362b7cde2c133bf0c8a6b8746b89d27335c8
c21070842 <mogridgeb@gmail.com>
2023-12-05 18:57:22 +0000

    added view request impl and initial html for view requests and sql data for testing reviews

--- src/main/java/uk/ac/cf/group5/Client/Project/Admin/ViewRequestsController.java
+++ src/main/java/uk/ac/cf/group5/Client/Project/Admin/ViewRequestsController.java
@@ -1,14 +1,25 @@
 package uk.ac.cf.group5.Client.Project.Admin;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import uk.ac.cf.group5.Client.Project.Reviews.RequestItem;
+import java.util.List;
 
 @Controller
 public class ViewRequestsController {
+    private ViewRequestsImpl viewRequestsImpl;
+
+
+    public ViewRequestsController(ViewRequestsImpl aViewRequestsImpl) {
+        this.viewRequestsImpl = aViewRequestsImpl;
+    }
     @GetMapping("Admin/ViewRequests")
     public ModelAndView getViewRequests(){
         ModelAndView ViewRequests = new ModelAndView("Admin/ViewRequests");
+        List<RequestItem> allRequestItems = viewRequestsImpl.getAllRequestItems();
+        ViewRequests.addObject("requestItems", allRequestItems);
         return ViewRequests;
     }
 }
