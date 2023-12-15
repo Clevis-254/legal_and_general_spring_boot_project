a192b562566b8cbb07207e8d3c1453e78ba50bbe
c22086220 <almeshariz@cardiff.ac.uk>
2023-12-15 14:54:57 +0000

    re-creating my tests after the updates in the classes

--- src/test/java/uk/ac/cf/group5/Client/Project/reviews/AddingReviewTest.java
+++ src/test/java/uk/ac/cf/group5/Client/Project/reviews/AddingReviewTest.java
@@ -1,48 +1,47 @@
-//package uk.ac.cf.group5.Client.Project.reviews;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.annotation.DirtiesContext;
-//import uk.ac.cf.group5.Client.Project.ReviewRequests.ReviewItem;
-//import uk.ac.cf.group5.Client.Project.ReviewRequests.ReviewRepoImp;
-//import uk.ac.cf.group5.Client.Project.Reviews.RequestItem;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest(webEnvironment = WebEnvironment.NONE)
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//class ReviewRepoImpTest {
-//
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//
-//    @Autowired
-//    private ReviewRepoImp reviewRepo;
-//
-//    @Test
-//    void addReview() {
-//
-//        RequestItem requestItem = new RequestItem();
-//        requestItem.setUserId(1L);
-//        requestItem.setId(2L);
-//
-//        reviewRepo.add(requestItem.getUserId(), requestItem.getId());
-//
-//
-//        int count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM reviews", Integer.class);
-//        assertEquals(1, count, "One row should be added to the Reviews table");
-//
-//
-//        List<ReviewItem> reviewItems = jdbcTemplate.query("SELECT * FROM reviews", reviewRepo.setReviewMapper());
-//        assertEquals(1, reviewItems.size(), "One review item should be added");
-//        ReviewItem addedReview = reviewItems.get(0);
-//        assertEquals(requestItem.getUserId(), addedReview.getUserId(), "User ID should match");
-//        assertEquals(requestItem.getId(), addedReview.getRequestId(), "Request ID should match");
-//
-//    }
-//}
+package uk.ac.cf.group5.Client.Project.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import uk.ac.cf.group5.Client.Project.ReviewRequests.ReviewItem;
+import uk.ac.cf.group5.Client.Project.ReviewRequests.ReviewRepoImp;
+
+import java.sql.Date;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ReviewRepoImpTest {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private ReviewRepoImp reviewRepo;
+
+    @Test
+    void getReviewItems() {
+        // Given
+        long userId = 1L;
+
+        List<ReviewItem> expectedReviewItems = Collections.singletonList(
+                new ReviewItem(1L, userId, 2L, "in_progress", Date.valueOf("2023-05-01"))
+        );
+        when(jdbcTemplate.query(any(String.class), any(RowMapper.class), any(Long.class)))
+                .thenReturn(expectedReviewItems);
+
+        // When
+        List<ReviewItem> actualReviewItems = reviewRepo.getReviewItems(userId);
+
+        // Then
+        assertEquals(expectedReviewItems, actualReviewItems,
+                "Review items should be retrieved as expected");
+    }
+}
